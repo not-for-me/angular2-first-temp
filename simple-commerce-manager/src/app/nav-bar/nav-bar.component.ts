21fbb781d33cf3d92dd6ced8573ed2490cc760e8
조우진 <woojin@tmon.co.kr>
2017-01-16 14:41:33 +0900

    finish default template

--- simple-commerce-manager/src/app/nav-bar/nav-bar.component.ts
+++ simple-commerce-manager/src/app/nav-bar/nav-bar.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  appTitle = '상품관리 시스템';
 
   constructor() { }
 
