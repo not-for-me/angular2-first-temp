62302f189ab82f79ab6dacc48795e5340fff1f10
조우진 <woojin@tmon.co.kr>
2017-01-17 19:29:30 +0900

    footer update and change sidebar icon

--- simple-commerce-manager/src/app/footer/footer.component.ts
+++ simple-commerce-manager/src/app/footer/footer.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  thisYear: number = new Date().getFullYear();
 
   constructor() { }
 
