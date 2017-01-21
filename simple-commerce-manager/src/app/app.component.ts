d3c1f8e1f4ec9a63a72e7568ec823aa6f5af0625
조우진 <woojin@tmon.co.kr>
2017-01-21 14:56:14 +0900

    formatting file

--- simple-commerce-manager/src/app/app.component.ts
+++ simple-commerce-manager/src/app/app.component.ts
@@ -1,10 +1,10 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-	selector: 'scm-root',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'scm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	curPage: string = 'default';
+  curPage: string = 'default';
 }
